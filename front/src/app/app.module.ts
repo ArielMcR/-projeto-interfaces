a7fdec8950b5265ef6955d6d19522f4f62a5857e
Ariel Machado <arielmachador3@gmail.com>
2022-09-17 22:46:32 -0300

    Eventos em Angular

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { FirstComponentComponent } from './components/first-component/first-component.component';
 import { ParentDataComponent } from './components/parent-data/parent-data.component';
 import { ClassesEIfComponent } from './components/classes-eif/classes-eif.component';
+import { EventosComponent } from './components/eventos/eventos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FirstComponentComponent,
     ParentDataComponent,
-    ClassesEIfComponent
+    ClassesEIfComponent,
+    EventosComponent
   ],
   imports: [
     BrowserModule
